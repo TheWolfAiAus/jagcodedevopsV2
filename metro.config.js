6070c47dec4883f7f0f3ef7df3656bf6005cd861
TheWolfAiAus <geddes@thewolfai.com.au>
2025-08-16 03:38:29 +1000

    COMMIT ALL

---
+++ metro.config.js
@@ -0,0 +1,16 @@
+﻿const { getDefaultConfig } = require('expo/metro-config');
+const path = require('path');
+
+/**
+ * Metro configuration for Expo
+ * - Adds resolver.alias to map '@' to the local 'src' directory so that
+ *   imports like `@/components/...` resolve to `./src/components/...`.
+ */
+const config = getDefaultConfig(__dirname);
+
+config.resolver.alias = {
+  ...(config.resolver.alias || {}),
+  '@': path.resolve(__dirname, 'src'),
+};
+
+module.exports = config;
