81902c39e4ef6cab87773c27727972cc201ddb5d
TheWolfAiAus <geddes@thewolfai.com.au>
2025-08-23 22:32:12 +1000

    commit

--- eslint.config.js
+++ eslint.config.js
@@ -1,21 +1,32 @@
-// https://docs.expo.dev/guides/using-eslint/
 const js = require('@eslint/js');
 const typescript = require('typescript-eslint');
+const reactHooks = require('eslint-plugin-react-hooks');
+const reactRefresh = require('eslint-plugin-react-refresh');
 
 module.exports = [
   js.configs.recommended,
   ...typescript.configs.recommended,
   {
-    ignores: ['dist/*', 'node_modules/*', 'build/*', '.expo/*', '**/*.js'],
+    ignores: ['dist/*', 'node_modules/*', 'build/*', '**/*.js'],
   },
   {
+    files: ['**/*.{ts,tsx}'],
     languageOptions: {
       ecmaVersion: 'latest',
       sourceType: 'module',
     },
+    plugins: {
+      'react-hooks': reactHooks,
+      'react-refresh': reactRefresh,
+    },
     rules: {
       '@typescript-eslint/no-unused-vars': 'warn',
       '@typescript-eslint/no-explicit-any': 'warn',
+      ...reactHooks.configs.recommended.rules,
+      'react-refresh/only-export-components': [
+        'warn',
+        { allowConstantExport: true },
+      ],
     }
   }
 ];
